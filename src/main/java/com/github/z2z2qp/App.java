ca535fcc48983a9488dae72d515ff70403925a55
庄佳彬 <zjbwill@dingtalk.com>
2022-11-21 15:47:06 +0800

    🎆init commit

---
+++ src/main/java/com/github/z2z2qp/App.java
@@ -0,0 +1,13 @@
+package com.github.z2z2qp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
