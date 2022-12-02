24d513bdeeb298d281fa880bc1f208c6b2ca3c53
庄佳彬 <zjbwill@dingtalk.com>
2022-12-02 13:30:21 +0800

    更新转换测试类

--- src/test/java/com/github/z2z2qp/tool/bean/CastTest.java
+++ src/test/java/com/github/z2z2qp/tool/bean/CastTest.java
@@ -3,6 +3,8 @@ package com.github.z2z2qp.tool.bean;
 import junit.framework.TestCase;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
 
 public class CastTest extends TestCase {
 
@@ -12,9 +14,21 @@ public class CastTest extends TestCase {
     }
 
     public void testTestCast() {
-        var list = List.of("1","2","3");
-        var list2 = Cast.cast(list, Integer::valueOf);
+        var list = List.of("1", "2", "3");
+        var list2 = Cast.cast(list, (Function<String, Integer>) Integer::valueOf);
         System.out.println(list.getClass());
         System.out.println(list2.getClass());
+    }
+
+    public void testTestCast2() {
+        var a = "1334";
+        var i = Cast.cast(a,Integer::valueOf);
+        System.out.println(i);
+    }
+
+    public void testListToMap() {
+        var list = List.of("1", "2", "3");
+        var map = Cast.listToMap(list, Objects::hashCode);
+        System.out.println(map);
     }
 }
